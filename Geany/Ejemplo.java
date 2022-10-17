b160e7c97f290cb7361e2df68c3acd27ebf41bdb
Pablomp94 <1744623@alu.murciaeduca.es>
2022-10-17 11:41:54 +0200

    Programación

---
+++ Geany/Ejemplo.java
@@ -0,0 +1,18 @@
+/*
+ * Autor : Pablo Morales Puertas
+ * Fecha : 14 / 10 / 22
+ * Descripción : Prueba del codigo java
+ */
+
+
+public class Ejemplo {
+	
+	public static void main (String[] args) {
+		
+		
+	 System.out.println("Hola Mundo");
+	 System.out.println("  ;;;;;\n;   ;\n;   ;\n;   ;\n;;;;;\n");
+		 
+	}
+}
+
---
+++ Visual Studio/Ejemplovisualestudio.java
@@ -0,0 +1,16 @@
+//* Online Java Compiler and Editor *//
+    public class HelloWorld{
+
+    public static void main(String []args){
+    System.out.println("HelloWorld") ;
+
+    int numero_a = 8 ;
+    int numero_b = 7 ;
+    int numero_c = 6 ; 
+    
+    if(numero_a>numero_b) {
+        System.out.println("A es > q b ");
+    }
+     System.out.println(";;;;;\n;   ;\n;   ;\n;   ;\n;;;;;\n") ; }
+
+}
