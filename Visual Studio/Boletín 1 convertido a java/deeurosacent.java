fac9a00a4996f5f362b4cbbd9b83a1983dc28881
Pablomp94 <1744623@alu.murciaeduca.es>
2022-10-18 13:39:11 +0200

    Programación

---
+++ Visual Studio/Boletín 1 convertido a java/deeurosacent.java
@@ -0,0 +1,11 @@
+//Hecho por: Pablo Morales Puertas NRE: 1744623
+//
+//
+//
+
+import java.util.Scanner; 
+    public class deeurosacent{
+        
+        System.out.println("Introduzca la canidad de euros")
+
+}
