7fd98662d2385588dda7e6ad241481faba495de9
Pablomp94 <1744623@alu.murciaeduca.es>
2022-10-24 13:31:40 +0200

    Programación

--- Visual Studio/Java codes/Adivinarnumeroaleatorio.java
+++ Visual Studio/Java codes/Adivinarnumeroaleatorio.java
@@ -20,6 +20,16 @@ public static void main(String[] args) {
     
     while (numero != aleatorio){
 
+        if (numero > aleatorio){
+
+            System.out.println("El numero introducido es mayor al numero a adivinar");
+        }
+        
+        if (numero < aleatorio){
+            
+            System.out.println("El numero introducido es menor al numero a adivinar");
+        }
+
         System.out.println("Vuelvelo a intentar");
         
         numero = sc.nextInt();
