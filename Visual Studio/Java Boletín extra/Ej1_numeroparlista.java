dfe72950b082ce1529c9983a81dddac77a7c76ed
Pablomp94 <1744623@alu.murciaeduca.es>
2022-11-14 11:41:38 +0100

    programación

--- Visual Studio/Java Boletín extra/Ej1_numeroparlista.java
+++ Visual Studio/Java Boletín extra/Ej1_numeroparlista.java
@@ -1,7 +1,7 @@
 //Pablo Morales Puertas NRE: 1744623//
 //Crear un contador del 1 al 200 que ponga solo los numeros pares sumandole 2. Haciendo uso del bucle for//
 
-public class numeroparlista{
+public class Ej1_numeroparlista{
 
     public static void main(String[] args){
 
