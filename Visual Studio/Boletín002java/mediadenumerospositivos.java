a74ea84a1f90f4329efc968bd2ca58c6978b64d4
Pablomp94 <1744623@alu.murciaeduca.es>
2022-11-21 14:20:56 +0100

    programación

---
+++ Visual Studio/Boletín002java/mediadenumerospositivos.java
@@ -0,0 +1,18 @@
+//Pablo Morales Puertas     NRE: 1744623//
+//Media de numeros , cuando se introduzca numero negativo para de poner numeros y realiza las cuentas//
+
+
+import java.util.Scanner;
+
+public class mediadenumerospositivos {
+ 
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+    }
+}
