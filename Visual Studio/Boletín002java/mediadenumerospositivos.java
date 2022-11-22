bab481cfd330ae4f74007a04119825d747ecf09a
Pablomp94 <1744623@alu.murciaeduca.es>
2022-11-22 09:17:56 +0100

    programación

--- Visual Studio/Boletín002java/mediadenumerospositivos.java
+++ Visual Studio/Boletín002java/mediadenumerospositivos.java
@@ -4,14 +4,49 @@
 
 import java.util.Scanner;
 
-public class mediadenumerospositivos {
+public class mediadenumerospositivos{
  
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
 
+        //variables//
 
+        int numero, media, suma, lista;
+
+    
+        lista = 0;
+
+        suma = 0; 
+
+        numero = 0;
+
+        do{
+            System.out.println("Introduce una serie de numeros para realizar la media, cuando hayas finalizado escribe un numero negativo");
+
+            numero = sc.nextInt();
+            
+            
+            if(numero >= 0){
+                lista ++;
+            
+                suma = numero + suma; 
+            }
+                
+            
+
+            System.out.println(suma);
+
+
+            System.out.println(lista);
+        
+            
+        }while(numero >= 0);
+
+        media = suma / lista ; 
+
+        System.out.println("La media de dichos numeros introducidos es de " + media);
 
 
     }
