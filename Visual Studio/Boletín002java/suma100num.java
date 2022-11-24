71cbd55a32f22527b2034fd79a383be7c089f8aa
Pablomp94 <1744623@alu.murciaeduca.es>
2022-11-24 08:59:18 +0100

    programación

---
+++ Visual Studio/Boletín002java/suma100num.java
@@ -0,0 +1,47 @@
+//Pablo Morales Puertas DNI: 1744623//
+//Poner un numero positivo y sumar sus 100 numeros siguientes//
+
+import java.util.Scanner;
+
+public class suma100num {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+            //Variables//
+
+        int numero, tope, suma; 
+
+        suma = 0;
+
+            //Codigo//
+
+        System.out.println("Introduce un numero para calcular la suma de sus 100 numeros siguientes");
+        
+        numero = sc.nextInt();
+        
+        tope = numero;
+    
+
+        while(numero <= -1){
+            System.out.println("El numero introducido debe de ser positivo");
+
+            numero = sc.nextInt();
+
+        }
+
+            
+        for( ; tope <= (numero + 99) ; tope ++){
+
+            suma = tope + suma;
+
+        }
+        
+    
+        System.out.println(tope);
+        System.out.println(suma);
+        
+        System.out.println("La suma de los 100 numeros entre " + numero + " y " + tope + " es " + suma); 
+    }
+}
