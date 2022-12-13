aa7964b9b421e3e4dbbba55758372cdbddc1ad60
Pablomp94 <1744623@alu.murciaeduca.es>
2022-12-13 08:56:34 +0100

    programación

---
+++ Visual Studio/UT02/Boletín002java/Ej09_Numprimo.java
@@ -0,0 +1,18 @@
+//Pablo Morales Puertas NRE: 1744623//
+//Fecha : 12/12/2022
+//Introducir numeros primos del 2 al 100
+
+public class Ej09_Numprimo {
+    
+    public static void main(String[] args) {
+        
+        int num;
+
+        num = 2;
+
+        for( ; num <= 100 ; num ++){
+
+            if(num )
+        }
+    }
+}
---
+++ Visual Studio/UT04/Ejermplo01/programaPrincipal.java
@@ -0,0 +1,9 @@
+package EJEMPLO01;
+
+public class programaPrincipal {
+    public static void main(String[] args) {
+        coche miToyota = new coche();
+        miToyota.repostar(30);
+        miToyota.conducir(300);
+    }
+}
