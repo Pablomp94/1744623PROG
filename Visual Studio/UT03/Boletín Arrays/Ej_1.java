d3e50627af33afa6688090655d66ff35c47a3210
Pablomp94 <1744623@alu.murciaeduca.es>
2022-12-16 11:33:11 +0100

    programación

---
+++ Visual Studio/UT03/Boletín Arrays/Ej_1.java
@@ -0,0 +1,20 @@
+//Pablo Morales Puertas NRE:1744623//
+//Crear un programa que almacene numeros en un array y le invierte las posiciones//
+
+import java.util.Scanner;
+
+public class Ej_1 {
+
+public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+
+
+    
+
+
+
+
+    }
+}
