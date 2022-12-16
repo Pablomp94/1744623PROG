53a000ebc71756d345bdf1c1d1693d6255a5cd66
Pablomp94 <1744623@alu.murciaeduca.es>
2022-12-16 12:10:36 +0100

    programación

--- Visual Studio/UT03/Boletín Arrays/Ej_1.java
+++ Visual Studio/UT03/Boletín Arrays/Ej_1.java
@@ -1,5 +1,5 @@
 //Pablo Morales Puertas NRE:1744623//
-//Crear un programa que almacene numeros en un array y le invierte las posiciones//
+//Crear un programa que almacene numeros en un array y rotar las posiciones//
 
 import java.util.Scanner;
 
@@ -7,7 +7,39 @@ public class Ej_1 {
 
 public static void main(String[] args){
 
-    Scanner sc = new Scanner(System.in);
+    int i; 
+
+    i = 0;
+
+    try (Scanner sc = new Scanner(System.in)) {
+        int num[] = new int[15];
+
+    System.out.println("Introduce 15 numeros");
+
+        for( ; i < num.length; i ++){
+
+            //System.out.println("Introduce el numero " +  + ":");
+            num[i] = sc.nextInt();
+        }
+
+        //Rotamos valores//
+
+        System.out.println("Rotamos valores:");
+
+        System.out.println("(");
+
+        System.out.println(num[14]);
+
+        for(i = 0; i < 14; i++){
+
+            System.out.println(num[(i)]);
+
+
+        }
+
+        System.out.println(")");
+        
+    }
 
 
 
