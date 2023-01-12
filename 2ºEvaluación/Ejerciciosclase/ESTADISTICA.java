b061575518f13e74bd6fa3ad39b7957a5f3d1b40
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-12 09:22:03 +0100

    programación

---
+++ 2ºEvaluación/Ejerciciosclase/ESTADISTICA.java
@@ -0,0 +1,23 @@
+public class ESTADISTICA {
+
+    private List<Int> listaNumeros;
+
+    public ESTADISTICA() {
+
+        listaNumeros = List.of(1, 2, 3, 4, 5, 4, 2, 8, 3, 4);
+
+    }
+
+    public Int mayor() {
+        return 8;
+    }
+
+    public Int menor() {
+        return 1;
+    }
+
+    public double media() {
+        return 3.6;
+    }
+
+}
