04d1cee4156d09f8027d06570bf6a667742379e5
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-13 11:28:51 +0100

    programación

--- 2ºEvaluación/Ejerciciosclase/Estadistica/App.java
+++ 2ºEvaluación/Ejerciciosclase/Estadistica/App.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class App {
- 
+
     public static void main(String[] args) {
-        
+
         int opcion;
-        
+
         Scanner sc = new Scanner(System.in);
 
         ESTADISTICA miESTADISTICA = new ESTADISTICA();
 
-        do{
+        do {
             System.out.println("****************");
             System.out.println("1.Saber el mayor");
             System.out.println("2.Saber el menor");
@@ -20,19 +20,19 @@ public class App {
 
             opcion = sc.nextInt();
 
-            if(opcion ==  1){
+            if (opcion == 1) {
                 System.out.println("El mayor es " + miESTADISTICA.mayor());
             }
 
-            if(opcion == 2){
+            if (opcion == 2) {
                 System.out.println("El menor es " + miESTADISTICA.menor());
             }
 
-            if(opcion == 3){
+            if (opcion == 3) {
                 System.out.println("La media es " + miESTADISTICA.media());
             }
 
-        }while(opcion != 4);
+        } while (opcion != 4);
 
         System.out.println("__________________");
         System.out.println("   HASTA PRONTO   ");
