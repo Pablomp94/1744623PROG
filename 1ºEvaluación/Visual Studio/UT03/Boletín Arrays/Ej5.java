018927cf591405b789c84476c77bbc881dccc67c
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-16 11:58:14 +0100

    programación

---
+++ 1ºEvaluación/Visual Studio/UT03/Boletín Arrays/Ej5.java
@@ -0,0 +1,14 @@
+//Pablo Morales Puertas NRE:1744623//
+//Crear dos arrays, uno con los valores del 1 al 100 y otro que sea el inverso del primer array//
+
+import java.util.*;
+
+public class Ej5 {
+    
+    public static void main(String[] args) {
+     
+        
+
+        
+    }
+}
