54986f7b523e68cd651a5574e5ed8b04151e020a
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-20 12:10:21 +0100

    programación

--- 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/Cuenta.java
+++ 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/Cuenta.java
@@ -1,35 +1,56 @@
 
 import java.util.*;
+import java.util.concurrent.atomic.DoubleAccumulator;
 
 public class Cuenta {
     
     Scanner sc = new Scanner(System.in);
 
-    int cuenta = 10000;
+    private double cuenta;
+    private ArrayList<Double> movimientos; 
 
-    ArrayList<Integer> movimientos = new ArrayList<Integer>();
+    public Cuenta(){
+        cuenta = 10000;
+        movimientos = new ArrayList<Double>();
+    }
 
     public void ingresarCuenta(){
 
-        int dinero = sc.nextInt();
+        System.out.println("Introduce el dinero a ingresar");
+
+        double dinero = sc.nextInt();
 
         cuenta = cuenta + dinero;
 
+        movimientos.add(dinero);
     }
     
     public void retirarCuenta(){
 
-        int dinero = sc.nextInt();
+        System.out.println("Introduce el dinero a retirar");
 
-        cuenta = cuenta - dinero;
+        double dinero = sc.nextInt();
 
+        if(dinero <= cuenta){
+
+            cuenta = cuenta - dinero;
+
+            movimientos.add(0-dinero);
+        }else{
+            System.out.println("No puedes retirar mas dinero del que tienes");
+        }
     }
 
 
-    public void consultarsaldoCuenta(){
+    public double consultarsaldoCuenta(){
 
-        System.out.println("En tu cuenta tienes actualmente " + cuenta + "$");
+        return cuenta;
+    }
 
+
+    public ArrayList<Double> movimientos(int opcion) {
+        
+        return movimientos;
     }
 
 
