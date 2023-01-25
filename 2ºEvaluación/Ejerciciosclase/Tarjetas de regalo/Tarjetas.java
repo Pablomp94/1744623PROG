9d1cbc8e46fb896cf943cc0bdff7020ff7589b0e
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-25 17:41:12 +0100

    programación

--- 2ºEvaluación/Ejerciciosclase/Tarjetas de regalo/Tarjetas.java
+++ 2ºEvaluación/Ejerciciosclase/Tarjetas de regalo/Tarjetas.java
@@ -16,6 +16,7 @@ public void getSaldo2(int saldo2){
 int total = saldo1 + saldo2;
 
 public void gastarsaldo(){
+    
     System.out.println(" ");
     System.out.println("Introduce el numero de tajetas que tienes:");
     System.out.println("1. UNA");
@@ -24,18 +25,48 @@ public void gastarsaldo(){
     opc = sc.nextInt();
 
     if(opc == 1){
-    System.out.println("En total tienes " + total + " euros.");
-    System.out.println("Ingresa el dinero que quieras gastar de las tarjetas:");
+        System.out.println("En total tienes " + total + " euros.");
+        System.out.println("Ingresa el dinero que quieras gastar de las tarjetas:");
 
-    gastar = sc.nextInt();
+        gastar = sc.nextInt();
 
-    total = total - gastar; 
+        total = total - gastar; 
 
-    System.out.println("Has gastado " + gastar + " euros, te quedan " + total + " euros." );
+        System.out.println("Has gastado " + gastar + " euros, te quedan " + total + " euros." );
     }else{
         System.out.println("Cual de las dos tarjetas quieres gastar?");
         System.out.println("1. Tarjeta numero uno: " + saldo1 + " $");
-        System.out.println("Tarjeta numero dos " + saldo2 + " $");
+        System.out.println("2. Tarjeta numero dos " + saldo2 + " $");
+
+        opc = sc.nextInt();
+
+        System.out.println(" ");
+        if(opc == 1){
+            System.out.println("Ingresa el dinero que quieras gastar de la tarjeta numero uno:");
+            gastar = sc.nextInt();
+            
+            if(gastar <= saldo1){
+                saldo1 = saldo1 - gastar;
+                System.out.println("Has gastado " + gastar + " euros, te quedan " + saldo1 + " euros en la tarjeta numero uno.");
+            }else{
+                System.out.println("No puedes introducir unos gastos mayores al dinero en la tarjeta");
+            }
+            System.out.println(" ");
+        }
+
+        if(opc == 2){
+            System.out.println("Ingresa el dinero que quieras gastar de la tarjeta numero uno:");
+            gastar = sc.nextInt();
+            
+            if(gastar <= saldo2){
+                saldo1 = saldo2 - gastar;
+                System.out.println("Has gastado " + gastar + " euros, te quedan " + saldo2 + " euros en la tarjeta numero dos.");
+            }else{
+                System.out.println("No puedes introducir unos gastos mayores al dinero en la tarjeta");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
 
