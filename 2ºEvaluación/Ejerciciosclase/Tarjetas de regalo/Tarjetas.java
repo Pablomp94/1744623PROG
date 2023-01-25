daf988b4599512bda5706da1a5777af9fad38dc4
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-25 17:33:19 +0100

    programación

--- 2ºEvaluación/Ejerciciosclase/Tarjetas de regalo/Tarjetas.java
+++ 2ºEvaluación/Ejerciciosclase/Tarjetas de regalo/Tarjetas.java
@@ -4,7 +4,7 @@ public class Tarjetas {
  
     Scanner sc = new Scanner(System.in);
 
-    private int saldo1, saldo2, saldo3, gastar;
+    private int saldo1, saldo2, saldo3, gastar, opc;
     
 
 public void getSaldo1(int saldo1){
@@ -16,7 +16,14 @@ public void getSaldo2(int saldo2){
 int total = saldo1 + saldo2;
 
 public void gastarsaldo(){
+    System.out.println(" ");
+    System.out.println("Introduce el numero de tajetas que tienes:");
+    System.out.println("1. UNA");
+    System.out.println("2. DOS");
 
+    opc = sc.nextInt();
+
+    if(opc == 1){
     System.out.println("En total tienes " + total + " euros.");
     System.out.println("Ingresa el dinero que quieras gastar de las tarjetas:");
 
@@ -24,8 +31,12 @@ public void gastarsaldo(){
 
     total = total - gastar; 
 
-    System.out.println("Has gastado " + gastar + " euros, te quedan " + " euros." );
-
+    System.out.println("Has gastado " + gastar + " euros, te quedan " + total + " euros." );
+    }else{
+        System.out.println("Cual de las dos tarjetas quieres gastar?");
+        System.out.println("1. Tarjeta numero uno: " + saldo1 + " $");
+        System.out.println("Tarjeta numero dos " + saldo2 + " $");
+    }
 }
 
 }
