57740acfbac016fb7c6874619b67b5ee92b9414d
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-31 14:06:13 +0100

    programación

---
+++ 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/Menu.java
@@ -0,0 +1,47 @@
+public class Menu {
+    
+
+    int opcion;
+
+    miMenu = new Menu();
+
+    public void MenuInicio(){
+        
+        do{
+
+        
+        System.out.println(" ");
+        System.out.println("********************************************************");
+        System.out.println("1.Consultar cuenta");
+        System.out.println("2.Ingresar dinero a la cuenta");
+        System.out.println("3.Retirar dinero de la cuenta");
+        System.out.println("4.Listado de movimientos realizados en la cuenta");
+        System.out.println("5.Salir");
+        System.out.println("********************************************************");
+
+        opcion = sc.nextInt();
+
+       
+        if (opcion == 1) {
+            System.out.println("El saldo de tu cuenta es de: " + miCuenta.consultarsaldoCuenta());
+        }
+
+        if (opcion == 2) {
+            miCuenta.ingresarCuenta();
+        }
+
+        if (opcion == 3) {
+            miCuenta.retirarCuenta();
+        }
+
+        if (opcion == 4){
+            System.out.println("Movimientos =" + miCuenta.movimientos(opcion));
+        }
+
+        }while(opcion != 5);
+                    
+        
+
+    }
+    
+}
