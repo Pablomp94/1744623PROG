a9ce241eaf66f81640ccc7f5ab6ed66e9990fb9b
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-31 14:24:01 +0100

    programación

---
+++ ENTORNOS/UT03/App de Metodos/App.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Ej_Metodos metodo = new Ej_Metodos();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un numero entero");
+
+        int num = sc.nextInt();
+
+        metodo.capicua(num);
+
+
+
+
+    }
+}
--- ENTORNOS/UT03/App de Metodos/Ej_Metodos.java
+++ ENTORNOS/UT03/App de Metodos/Ej_Metodos.java
@@ -1,38 +1,12 @@
 import java.util.*;
 
-
 public class Ej_Metodos {
-    
 
     Scanner sc = new Scanner(System.in);
 
-
-    public void capicua(){
+    public void capicua(int num) {
 
         
-
-
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 }
-
