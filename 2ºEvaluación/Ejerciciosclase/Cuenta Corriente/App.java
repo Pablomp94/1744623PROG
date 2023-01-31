48793d497a0699ab092ccc304d23169e4771f9a9
Pablomp94 <1744623@alu.murciaeduca.es>
2023-01-31 09:24:59 +0100

    programación

--- 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/App.java
+++ 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/App.java
@@ -5,42 +5,9 @@ public class App {
 
         Cuenta miCuenta = new Cuenta();
 
-        int opcion;
-
         Scanner sc = new Scanner(System.in);
 
-        do {
-            System.out.println(" ");
-            System.out.println("********************************************************");
-            System.out.println("1.Consultar cuenta");
-            System.out.println("2.Ingresar dinero a la cuenta");
-            System.out.println("3.Retirar dinero de la cuenta");
-            System.out.println("4.Listado de movimientos realizados en la cuenta");
-            System.out.println("5.Salir");
-            System.out.println("********************************************************");
-
-            opcion = sc.nextInt();
-
-            if (opcion == 1) {
-                System.out.println("El saldo de tu cuenta es de: " + miCuenta.consultarsaldoCuenta());
-            }
-            ;
-
-            if (opcion == 2) {
-                miCuenta.ingresarCuenta();
-            }
-
-            if (opcion == 3) {
-                miCuenta.retirarCuenta();
-            }
-
-            if (opcion == 4){
-                System.out.println("Movimientos =" + miCuenta.movimientos(opcion));
-            }
-
-        } while (opcion != 5);
-                
-        System.out.println("Has salido");
-            
+        miCuenta.MenuInicio();
+                 
     }
 }
--- 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/Cuenta.java
+++ 2ºEvaluación/Ejerciciosclase/Cuenta Corriente/Cuenta.java
@@ -1,18 +1,23 @@
 import java.util.*;
-import java.util.concurrent.atomic.DoubleAccumulator;
+
+
 
 public class Cuenta {
-    
+
+    Cuenta miCuenta = new Cuenta();
+
     Scanner sc = new Scanner(System.in);
 
     private double cuenta;
     private ArrayList<Double> movimientos; 
+    int opcion;
 
     public Cuenta(){
         cuenta = 10000;
         movimientos = new ArrayList<Double>();
     }
 
+   
     public void ingresarCuenta(){
 
         System.out.println("Introduce el dinero a ingresar");
@@ -52,5 +57,44 @@ public class Cuenta {
         return movimientos;
     }
 
+    public void MenuInicio(){
+        
+        do{
+
+        
+        System.out.println(" ");
+        System.out.println("********************************************************");
+        System.out.println("1.Consultar cuenta");
+        System.out.println("2.Ingresar dinero a la cuenta");
+        System.out.println("3.Retirar dinero de la cuenta");
+        System.out.println("4.Listado de movimientos realizados en la cuenta");
+        System.out.println("5.Salir");
+        System.out.println("********************************************************");
+
+        opcion = sc.nextInt();
+
+       
+        if (opcion == 1) {
+            System.out.println("El saldo de tu cuenta es de: " + miCuenta.consultarsaldoCuenta());
+        }
+
+        if (opcion == 2) {
+            miCuenta.ingresarCuenta();
+        }
+
+        if (opcion == 3) {
+            miCuenta.retirarCuenta();
+        }
+
+        if (opcion == 4){
+            System.out.println("Movimientos =" + miCuenta.movimientos(opcion));
+        }
+
+        }while(opcion != 5);
+                    
+        
+
+    }
+    
 
 }
