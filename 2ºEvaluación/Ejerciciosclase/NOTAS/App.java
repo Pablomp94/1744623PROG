21245b7cff48493bbc19b1a7684c3821ae1f7b06
Pablomp94 <1744623@alu.murciaeduca.es>
2023-02-03 12:02:51 +0100

    programación

---
+++ 2ºEvaluación/Ejerciciosclase/NOTAS/App.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        
+        ArrayList<String> lista = new ArrayList <String>();
+
+        
+        
+
+    }
+}
