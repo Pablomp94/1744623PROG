b76817004bb27566fbf0a8bb07725577a344a774
Pablomp94 <1744623@alu.murciaeduca.es>
2023-02-14 13:06:06 +0100

    programación

---
+++ 2ºEvaluación/Ejerciciosclase/Ejercicio 9/Alumno.java
@@ -0,0 +1,20 @@
+public class Alumno extends Persona {
+    
+    private int Nia;
+
+    public Alumno(){
+
+    }
+
+    public int getNia() {
+        return Nia;
+    }
+
+    public void setNia(int nia) {
+        Nia = nia;
+    }
+
+    
+
+
+}
