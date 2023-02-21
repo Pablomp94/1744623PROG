34fca80516fcae77add8b2d386e442618e60b86a
Pablomp94 <1744623@alu.murciaeduca.es>
2023-02-21 19:35:40 +0100

    programación

---
+++ 2ºEvaluación/Boletín ArrayList/Ej2.java
@@ -0,0 +1,9 @@
+//Pablo Morales Puertas NRE: 1744623//
+//Escribe un programa Java para iterar a través de todos los elementos en una ArrayList.//
+
+public class Ej2 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ 2ºEvaluación/Boletín ArrayList/Ej3.java
@@ -0,0 +1,9 @@
+//Pablo Morales Puertas NRE: 1744623//
+//Escribe un programa Java para insertar un elemento en la ArrayList en la primera posición.//
+
+
+public class Ej3 {
+    public static void main(String[] args) {
+        
+    }
+}
