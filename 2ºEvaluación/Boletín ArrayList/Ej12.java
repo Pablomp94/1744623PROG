eebfaa51809fa0c725cc81e61b8cacd75cb0d0aa
Pablomp94 <1744623@alu.murciaeduca.es>
2023-02-27 18:00:35 +0100

    programación

--- 2ºEvaluación/Boletín ArrayList/Ej12.java
+++ 2ºEvaluación/Boletín ArrayList/Ej12.java
@@ -1,8 +1,34 @@
 //Pablo Morales Puertas NRE: 1744623//
 //Escribe un programa Java para extraer una parte de una ArrayList.//
 
+import java.util.*;
+
 public class Ej12 {
-    public static void main(String[] args) {
-        
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    ArrayList<String> list = new ArrayList<>(
+      List.of("Pepe", "Maria", "Juan", "Jose", "Pedro")
+    );
+    try {
+      System.out.println(list);
+
+      System.out.println(
+        "Introduce hasta que elemento quieres eliminar del arrayList, incluyendose este"
+      );
+
+      int pos = sc.nextInt();
+
+      for (int i = 0; i < pos; i++) {
+        list.remove(0);
+      }
+
+      System.out.println(list);
+    } catch (java.lang.IndexOutOfBoundsException e) {
+      System.out.println(
+        "Ha ocurrido un error, compruebe los valores introducidos"
+      );
     }
+  }
 }
