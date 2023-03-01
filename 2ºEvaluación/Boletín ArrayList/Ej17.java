7c79fea0435b3f8321f16f6544eb3095ba93129e
Pablomp94 <1744623@alu.murciaeduca.es>
2023-03-01 18:34:36 +0100

    programación

---
+++ 2ºEvaluación/Boletín ArrayList/Ej17.java
@@ -0,0 +1,8 @@
+//Pablo Morales Puertas NRE:1744623//
+//Escribe un programa Java para vaciar un ArrayList.//
+
+public class Ej17 {
+    public static void main(String[] args) {
+        
+    }
+}
