cfa0052533d779fc32ad0a64ed2fc1e1b80acd01
Pablomp94 <1744623@alu.murciaeduca.es>
2023-03-05 18:54:13 +0100

    programación

--- 2ºEvaluación/Boletín ArrayList/Ej17.java
+++ 2ºEvaluación/Boletín ArrayList/Ej17.java
@@ -1,8 +1,23 @@
 //Pablo Morales Puertas NRE:1744623//
 //Escribe un programa Java para vaciar un ArrayList.//
 
+import java.util.*;
+
 public class Ej17 {
     public static void main(String[] args) {
         
+        ArrayList<String> list =  new ArrayList<> (List.of("Pepe", "Maria", "Juan", "Jose" , "Pedro"));
+
+        System.out.println(list);
+
+        int limit = list.size();
+
+        //Bucle que se realiza en numero de veces igual a la longitud del array, por cada ves que se realiza elimina la primera posición del array//
+
+        for(int i = 0; i < limit; i++){
+            list.remove(0);
+        }
+
+        System.out.println(list);
     }
 }
