859c8bfec8a2c3a96f8402788003c5f1f46263fb
Pablomp94 <1744623@alu.murciaeduca.es>
2023-03-27 16:42:00 +0200

    programación

---
+++ 3ºEVALUACION/demo/src/main/java/es/ieslosalbares/demo/Persona.java
@@ -0,0 +1,65 @@
+package es.ieslosalbares.demo;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Persona")
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    private String apellidos; 
+    private int numTelefono;
+    private String email;
+    private String fechaNacimiento;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public int getNumTelefono() {
+        return numTelefono;
+    }
+    public void setNumTelefono(int numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(String string) {
+        this.fechaNacimiento = string;
+    }
+
+
+    @Override
+    public String toString() {
+        return getNombre() + ";" +
+        getApellidos() + ";" +
+        getNumTelefono() + ";" +
+        getEmail() + ";" +
+        getFechaNacimiento()+ ";\n" ;
+    }
+
+    
+}
---
+++ 3ºEVALUACION/demo/src/main/java/es/ieslosalbares/demo/PersonaRepositorio.java
@@ -0,0 +1,11 @@
+package es.ieslosalbares.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PersonaRepositorio extends CrudRepository<Persona , Long> {
+
+    
+
+}
